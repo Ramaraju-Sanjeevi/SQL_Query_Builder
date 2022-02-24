50acc4e114397ec9d60c9b1b0972be578011aef5
Ramaraju-Sanjeevi <ramaraju.sanjeevi06@gmail.com>
2022-02-24 22:30:27 +0530

    removed test service files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
-import { CustomerService } from './customservices/customerservice.service';
 import { TableDataServiceService } from './customservices/table-data-service.service'
 import {FormsModule} from '@angular/forms';
 import {TableModule} from 'primeng/table';
@@ -28,7 +27,7 @@ import { CardModule } from 'primeng/card';
     AppRoutingModule, HttpClientModule,
     DropdownModule, TableModule, InputTextModule, ButtonModule, CardModule
   ],
-  providers: [CustomerService, TableDataServiceService],
+  providers: [TableDataServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
